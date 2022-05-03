3237b39b30f09595b9db209c2fe4878adb6b7924
ttzein6 <ttzein6@gmail.com>
2022-05-03 11:46:01 +0300

    added filter

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,36 @@
 let url = 'https://filltext.com/?rows=10&fname={firstName}&lname={lastName}&category=[%22category1%22,%22category2%22,%22category3%22]&pretty=true';
-let output = {};
-fetch(url)
-    .then(res => res.json())
-    .then((out) => {
-        showCard(out);
-}).catch(err => console.error(err));
+let categories = ["category1", "category2", "category"];
+fetch(url).then(res => res.json()).then((out) => showCard(out) );
+
 
 showCard = (out) => {
     const cardDiv = document.querySelector('.cards');
+    console.log(out)
     out.forEach(item => {
         const cardElement = document.createElement('div');
         cardElement.classList.add('card');
+        cardElement.classList.add(item.category);
         cardElement.innerHTML = `<div class="cardlogo">
         ${item.fname[0]}${item.lname[0]}
     </div>
     <div class="fname">${item.fname}</div>
     <div class="lname">${item.lname}</div>
-    <spam class="category ${item.category}">${item.category}</div>`;
+    <spam class="category">${item.category}</div>`;
         cardDiv.appendChild(cardElement);
     })
 }
+
+
+
+
+
 
 let category1 = document.getElementById('category1');
 let category2 = document.getElementById('category2');
 let category3 = document.getElementById('category3');
 
 category1.addEventListener('click', () => {
-    if('active' in category1.classList){
+    if(category1.classList.contains('active')){
         category1.classList.remove('active');
         let hideCategory1 = document.getElementsByClassName('category1');
         for(let i = 0; i < hideCategory1.length; i++){
@@ -37,14 +41,14 @@ category1.addEventListener('click', () => {
         category1.classList.add('active');
         let showCategory1 = document.getElementsByClassName('category1');
         for(let i = 0; i < showCategory1.length; i++){
-            showCategory1[i].style.display = 'auto';
+            showCategory1[i].style.display = 'flex';
         }
     }
 });
 
 
 category2.addEventListener('click', () => {
-    if('active' in category2.classList){
+    if(category2.classList.contains('active')){
         category2.classList.remove('active');
         let hideCategory2 = document.getElementsByClassName('category2');
         for(let i = 0; i < hideCategory2.length; i++){
@@ -55,12 +59,12 @@ category2.addEventListener('click', () => {
         category2.classList.add('active');
         let showCategory2 = document.getElementsByClassName('category2');
         for(let i = 0; i < showCategory2.length; i++){
-            showCategory2[i].style.display = 'auto';
+            showCategory2[i].style.display = 'flex';
         }
     }
 });
 category3.addEventListener('click', () => {
-    if('active' in category3.classList){
+    if(category3.classList.contains('active')){
         category3.classList.remove('active');
         let hideCategory3 = document.getElementsByClassName('category3');
         for(let i = 0; i < hideCategory3.length; i++){
@@ -71,7 +75,7 @@ category3.addEventListener('click', () => {
         category3.classList.add('active');
         let showCategory3 = document.getElementsByClassName('category3');
         for(let i = 0; i < showCategory3.length; i++){
-            showCategory3[i].style.display = 'auto';
+            showCategory3[i].style.display = 'flex';
         }
     }
 });
